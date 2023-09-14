6ff5d0179d8b4b5df0c3935dc5ee52605a385e2e
tim <callmetimtim@gmail.com>
2023-09-14 17:40:19 +0800

    change py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 from llama_cpp import Llama
 from zhconv import convert
 
-llm = Llama(model_path="D:/Downloads/chinese-llama-2-7b-16k/ggml-model-f16.gguf")
+# llm = Llama(model_path="D:/Downloads/chinese-llama-2-7b-16k/ggml-model-f16.gguf")
+llm = Llama(model_path="../llama.cpp/models/chinese-llama-2-7b-16k/ggml-model-f16.gguf")
 # output = llm("什麽是LlaMa？", max_tokens=1024, stop=["Q:", "\n"], echo=True)
-while True:
-    human = convert(input("Enter: "), 'zh-hans')
-    print("waiting for response...")
-    output = llm(human, max_tokens=0)
-    print(output)
-    print(output["choices"][0]["text"])
+output = llm("你好，你是誰", max_tokens=0)
+print(output)
+print(output["choices"][0]["text"])
